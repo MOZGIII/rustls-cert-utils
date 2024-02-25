681de141ec187efb71d2e80d34eb22484f3bb0e7
MOZGIII <mike-n@narod.ru>
2024-02-25 06:35:23 -0300

    More explicit and flexible resolver error type

--- crates/rustls-cert-reloadable-resolver/src/loader.rs
+++ crates/rustls-cert-reloadable-resolver/src/loader.rs
@@ -1,5 +1,7 @@
 //! The [`CertifiedKeyLoader`].
 
+use futures_util::TryFutureExt as _;
+
 /// Load the [`rustls::sign::CertifiedKey`] from the specified paths using the specified readers.
 #[derive(Debug)]
 pub struct CertifiedKeyLoader<KeyProvider, KeyReader, CertsReader> {
@@ -13,38 +15,48 @@ pub struct CertifiedKeyLoader<KeyProvider, KeyReader, CertsReader> {
 
 /// An error that can occur while loading the data.
 #[derive(Debug, thiserror::Error)]
-pub enum CertifiedKeyLoaderError<R: std::error::Error + 'static> {
-    /// Reading failed.
-    #[error("read: {0}")]
-    Read(R),
+pub enum CertifiedKeyLoaderError<ReadKey, ReadCerts> {
+    /// Reading the key failed.
+    #[error("reading key: {0}")]
+    ReadKey(ReadKey),
+    /// Reading the certificate failed.
+    #[error("reading certs: {0}")]
+    ReadCerts(ReadCerts),
     /// Key processing failed.
-    #[error("key: {0}")]
-    Key(rustls::Error),
+    #[error("loading key: {0}")]
+    LoadKey(rustls::Error),
 }
 
 #[async_trait::async_trait]
-impl<KeyProvider, KeyReader, CertsReader, E> rustls_cert_reloadable::Loader
+impl<KeyProvider, KeyReader, CertsReader> rustls_cert_reloadable::Loader
     for CertifiedKeyLoader<KeyProvider, KeyReader, CertsReader>
 where
     KeyProvider: rustls::crypto::KeyProvider,
-    KeyReader: rustls_cert_read::ReadKey<Error = E> + Send,
-    CertsReader: rustls_cert_read::ReadCerts<Error = E> + Send,
-    E: std::error::Error + Send + 'static,
+    KeyReader: rustls_cert_read::ReadKey + Send,
+    CertsReader: rustls_cert_read::ReadCerts + Send,
+    KeyReader::Error: std::error::Error + Send + 'static,
+    CertsReader::Error: std::error::Error + Send + 'static,
 {
     type Value = rustls::sign::CertifiedKey;
-    type Error = CertifiedKeyLoaderError<E>;
+    type Error = CertifiedKeyLoaderError<KeyReader::Error, CertsReader::Error>;
 
     async fn load(&mut self) -> Result<Self::Value, Self::Error> {
         let (certs, key) = {
-            let key_fut = self.key_reader.read_key();
-            let certs_fut = self.certs_reader.read_certs();
-            tokio::try_join!(certs_fut, key_fut).map_err(CertifiedKeyLoaderError::Read)?
+            let key_fut = self
+                .key_reader
+                .read_key()
+                .map_err(CertifiedKeyLoaderError::ReadKey);
+            let certs_fut = self
+                .certs_reader
+                .read_certs()
+                .map_err(CertifiedKeyLoaderError::ReadCerts);
+            tokio::try_join!(certs_fut, key_fut)?
         };
 
         let key = self
             .key_provider
             .load_private_key(key)
-            .map_err(CertifiedKeyLoaderError::Key)?;
+            .map_err(CertifiedKeyLoaderError::LoadKey)?;
 
         Ok(rustls::sign::CertifiedKey::new(certs, key))
     }
