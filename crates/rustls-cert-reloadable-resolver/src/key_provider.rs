06ad84e6c8177f061bda2ba7d1ec656c34c8d1cd
MOZGIII <mike-n@narod.ru>
2024-02-25 08:12:48 -0300

    Add CryptoProvider key provider (#12)

--- crates/rustls-cert-reloadable-resolver/src/key_provider.rs
+++ crates/rustls-cert-reloadable-resolver/src/key_provider.rs
@@ -13,3 +13,32 @@ impl rustls::crypto::KeyProvider for Dyn {
         self.0.load_private_key(key_der)
     }
 }
+
+/// A wrapper for anything that can borrow [`rustls::crypto::CryptoProvider`] that implements
+/// [`rustls::crypto::KeyProvider`].
+pub struct FromCrypto<T>(pub T)
+where
+    T: std::borrow::Borrow<rustls::crypto::CryptoProvider>;
+
+impl<T> rustls::crypto::KeyProvider for FromCrypto<T>
+where
+    T: std::borrow::Borrow<rustls::crypto::CryptoProvider> + Send + Sync,
+{
+    fn load_private_key(
+        &self,
+        key_der: rustls::pki_types::PrivateKeyDer<'static>,
+    ) -> Result<std::sync::Arc<dyn rustls::sign::SigningKey>, rustls::Error> {
+        let crypto_provider: &rustls::crypto::CryptoProvider = self.0.borrow();
+        crypto_provider.key_provider.load_private_key(key_der)
+    }
+}
+
+impl<T> std::fmt::Debug for FromCrypto<T>
+where
+    T: std::borrow::Borrow<rustls::crypto::CryptoProvider>,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let crypto_provider: &rustls::crypto::CryptoProvider = self.0.borrow();
+        f.debug_tuple("FromCrypto").field(crypto_provider).finish()
+    }
+}
