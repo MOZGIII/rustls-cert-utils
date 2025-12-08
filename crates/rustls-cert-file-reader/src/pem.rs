5f46351e80bae65930f31fa116a64177af0ae7b3
Frando <frando@unbiskant.org>
2025-12-08 13:14:25 +0100

    fix: restore original key parsing

--- crates/rustls-cert-file-reader/src/pem.rs
+++ crates/rustls-cert-file-reader/src/pem.rs
@@ -14,19 +14,26 @@ pub fn parse_certs(rd: &mut dyn io::BufRead) -> Result<Vec<CertificateDer<'stati
         .map_err(into_io_err)
 }
 
-/// Parse the signle private key from PEM (PKCS8).
+/// Parse the single private key from PEM (PKCS8).
 pub fn parse_key(rd: &mut dyn io::BufRead) -> Result<PrivateKeyDer<'static>, io::Error> {
-    let key = PrivateKeyDer::from_pem_reader(&mut *rd).map_err(into_io_err)?;
-    // Assert there are no more keys present in the data.
-    while let Some((kind, data)) = pem::from_buf(&mut *rd).map_err(into_io_err)? {
-        if let Some(_) = PrivateKeyDer::from_pem(kind, data) {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidInput,
-                "more than one key".to_string(),
-            ));
-        }
+    let mut iter = PrivateKeyDer::pem_reader_iter(rd);
+    let first_key = match iter.next() {
+        Some(key) => key.map_err(into_io_err),
+        None => Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            "no key found in the given data".to_string(),
+        )),
+    };
+
+    // Assert there are no more keys present in the data
+    if iter.next().is_some() {
+        Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "more than one key".to_string(),
+        ))
+    } else {
+        first_key
     }
-    Ok(key)
 }
 
 fn into_io_err(err: pem::Error) -> io::Error {
