adaf9332220d48f77fb63ca14e792262af005b5d
MOZGIII <mike-n@narod.ru>
2025-12-08 20:41:39 +0400

    Drop rustls-pemfile dependency (#23)

--- crates/rustls-cert-file-reader/src/pem.rs
+++ crates/rustls-cert-file-reader/src/pem.rs
@@ -2,8 +2,6 @@
 
 use std::io::ErrorKind;
 
-use rustls_pemfile::{read_one, Item};
-
 /// Parse the certificates from PEM.
 pub fn parse_certs(
     rd: &mut dyn std::io::BufRead,
@@ -14,13 +12,13 @@ pub fn parse_certs(
         let Some(item) = read_one(rd)? else {
             return Ok(certs);
         };
-        if let Item::X509Certificate(cert) = item {
+        if let Some(cert) = cert_from_pemfile_item(item) {
             certs.push(cert);
         }
     }
 }
 
-/// Parse the signle private key from PEM (PKCS8).
+/// Parse the single private key from PEM (PKCS8).
 pub fn parse_key(
     rd: &mut dyn std::io::BufRead,
 ) -> Result<rustls_pki_types::PrivateKeyDer<'static>, std::io::Error> {
@@ -53,15 +51,35 @@ pub fn parse_key(
     Ok(key)
 }
 
+/// A type representing a generic PEM file data item.
+type PemObjectTuple = (rustls_pki_types::pem::SectionKind, Vec<u8>);
+
+/// Read one item from a PEM file.
+fn read_one(rd: &mut dyn std::io::BufRead) -> Result<Option<PemObjectTuple>, std::io::Error> {
+    match rustls_pki_types::pem::from_buf(rd) {
+        Ok(item) => Ok(item),
+        Err(rustls_pki_types::pem::Error::Io(error)) => Err(error),
+        // Should not be possible, but we fallback here if it actually happens.
+        Err(error) => Err(std::io::Error::other(error)),
+    }
+}
+
 /// Obtain the private key from a the pemfile item.
 /// If the item is not a private key - returns None.
 fn private_key_from_pemfile_item(
-    item: rustls_pemfile::Item,
+    (kind, der): PemObjectTuple,
 ) -> Option<rustls_pki_types::PrivateKeyDer<'static>> {
-    Some(match item {
-        Item::Pkcs1Key(key) => key.into(),
-        Item::Pkcs8Key(key) => key.into(),
-        Item::Sec1Key(key) => key.into(),
-        _ => return None,
-    })
+    <rustls_pki_types::PrivateKeyDer<'static> as rustls_pki_types::pem::PemObject>::from_pem(
+        kind, der,
+    )
+}
+
+/// Obtain the certificate from a the pemfile item.
+/// If the item is not a certificate - returns None.
+fn cert_from_pemfile_item(
+    (kind, der): PemObjectTuple,
+) -> Option<rustls_pki_types::CertificateDer<'static>> {
+    <rustls_pki_types::CertificateDer<'static> as rustls_pki_types::pem::PemObject>::from_pem(
+        kind, der,
+    )
 }
